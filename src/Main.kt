e2b77593fa8970965ed3a944ec899f651a553b85
gsn82 <gsn82@mail.ru>
2020-08-30 10:39:39 +0500

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println("hello world")
+    println(add(6, 5))
+
+    var i =5.1
+    val j="hello world";
+    i= 5.4
+    
+}
+
+fun add(a: Int, b: Int) = a + b
